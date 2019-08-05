2f68979ed8ffdbec3952734ea42be65e0c3596e5
nishi.singh <nishi.singh@cgi.com>
2019-08-05 11:56:17 +0530

    property task completed

--- src/main/java/com/stackroute/dataseeder/TrackServiceApplicationListener.java
+++ src/main/java/com/stackroute/dataseeder/TrackServiceApplicationListener.java
@@ -4,38 +4,51 @@ import com.stackroute.domain.Track;
 import com.stackroute.exceptions.TrackAlreadyExistsException;
 import com.stackroute.service.TrackService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TrackServiceApplicationListener implements ApplicationListener<ContextRefreshedEvent> {
-    public TrackService trackService;
+@PropertySource("classpath:application.properties")
+//used to specify the path of the resource file.
+class ApplicationListerSeedata implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Qualifier("trackService")
+    //Used to particularly mention the bean name.
+    private TrackService trackService;
 
     @Autowired
-    public  TrackServiceApplicationListener(TrackService trackService)
-    {
-        this.trackService=trackService;
+    public ApplicationListerSeedata(TrackService trackService) {
+        this.trackService = trackService;
     }
 
+    @Value("${track1.id}")
+    private int trackId;
+    @Value("${track1.name}")
+    private String trackName;
+    @Value("${track1.comments}")
+    private String trackComments;
+
+    @Autowired
+    private Environment environment;
 
     @Override
-    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-        Track track1 = new Track(1, "Track 1", "Comment 1");
-        Track track2=new Track(2,"track 2","comment 2");
-        Track track3=new Track(3,"track 3","comment 3");
-        try{
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent){
+        Track track1 = new Track(trackId, trackName, trackComments);
+        Track track2 = new Track(Integer.parseInt(environment.getProperty("track2.id")),
+                environment.getProperty("track2.name"), environment.getProperty("track2.comments"));
+        try
+        {
             trackService.saveTrack(track1);
             trackService.saveTrack(track2);
-            trackService.saveTrack(track3);
-
-        }catch(TrackAlreadyExistsException trackAlreadyExistsException )
-        {
-            trackAlreadyExistsException.printStackTrace();
+        }catch (TrackAlreadyExistsException ex){
+            ex.printStackTrace();
         }
-
-
     }
 }
--- src/main/java/com/stackroute/dataseeder/TrackServiceSeedDataCommandLineRunner.java
+++ src/main/java/com/stackroute/dataseeder/TrackServiceSeedDataCommandLineRunner.java
@@ -2,38 +2,45 @@ package com.stackroute.dataseeder;
 
 import com.stackroute.domain.Track;
 import com.stackroute.exceptions.TrackAlreadyExistsException;
+import com.stackroute.repository.TrackRepository;
 import com.stackroute.service.TrackService;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TrackServiceSeedDataCommandLineRunner implements CommandLineRunner {
-    TrackService trackService;
-
+@Data
+//To create getters and setters for all properties, to override toString(),
+// to create equals, canEquals and HashCode.
+@ConfigurationProperties(prefix = "track4")
+//Used to get the data prefix with track4 and matches with the property name.
+ class CommandLineRunnerSeedData implements CommandLineRunner {
+    @Qualifier("trackService")
+    //Used to particularly mention the bean name.
+    private TrackRepository trackRepository;
 
     @Autowired
-    public TrackServiceSeedDataCommandLineRunner(TrackService trackService)
-    {
-        this.trackService=trackService;
-
+    public CommandLineRunnerSeedData(TrackRepository trackRepository) {
+        this.trackRepository = trackRepository;
     }
 
+    private int id;
+    private String name;
+    private String comments;
+
+    @Autowired
+    private Environment environment;
 
     @Override
     public void run(String... args) throws Exception {
-        Track track1 = new Track(11, "Track 11", "Comment from CommandLineRunner");
-        Track track2=new Track(12,"Track 12","Comment from CommandLineRnnner");
-        Track track3=new Track(13,"Track 13","Comment from CommandLineRunner");
-        try {
-            trackService.saveTrack(track1);
-            trackService.saveTrack(track2);
-            trackService.saveTrack(track3);
-        } catch (TrackAlreadyExistsException trackAlreadyExistsException) {
-            trackAlreadyExistsException.printStackTrace();
-
-        }
-
-
+        Track track1 = new Track(Integer.parseInt(environment.getProperty("track3.id")),
+                environment.getProperty("track3.name"), environment.getProperty("track3.comments"));
+        Track track2 = new Track(id, name, comments);
+        trackRepository.save(track1);
+        trackRepository.save(track2);
     }
 }
